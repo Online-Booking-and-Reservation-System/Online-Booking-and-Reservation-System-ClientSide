4e2c2654f8df2c7545f3a066041892530f83268f
Amera Ehab <103929057+ameraehab@users.noreply.github.com>
2024-10-18 22:01:06 +0300

    Update resturantDetails.jsx

--- frontend/src/pages/user pages/restaurantDetails/resturantDetails.jsx
+++ frontend/src/pages/user pages/restaurantDetails/resturantDetails.jsx
@@ -1,13 +1,40 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faLocationDot} from '@fortawesome/free-solid-svg-icons';
 import './resturantDetails.css';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
 
+ function resturantDetails(){
+    const [restaurantDetails,setRestaurantDetails]=useState(null);
+    const { id } = useParams(); 
 
-export default function resturantDetails(){
+useEffect(()=>{
+    async function getRestaurantDetails() {
+        try {
+            const res = await axios.get(`http://localhost:3000/api/resturants/${id}`);
+            setRestaurantDetails(res.data.data.resturants)
+        } catch (error) {
+            
+        }
+    }
+    getRestaurantDetails();
+},[id]);
+
     return(
-<div className='container'>
-<div className='restaurant-window-Details'></div>
+        <>
+        <div className='container'>
+<div className='.restaurants-window-Details'></div>
+<h2><FontAwesomeIcon icon="fa-solid fa-arrow-left" />Go Back</h2>
+<img src={restaurantDetails.imgUrl} alt={restaurantDetails.restaurantName}/>
 
+<h2>{restaurantDetails.restaurantName}</h2>
+<p>{restaurantDetails.fullAddress}</p>
+            <p>Open Hours: {restaurantDetails.openTime}:00 - {restaurantDetails.closeTime}:00</p>
+            <p>{restaurantDetails.description}</p>
 </div>
+        </>
+
     );
 }
+
+export default resturantDetails;

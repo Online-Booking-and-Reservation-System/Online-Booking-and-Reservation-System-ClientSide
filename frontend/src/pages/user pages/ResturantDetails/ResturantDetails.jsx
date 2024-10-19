6fd02b897f86e733b697f6207643f6f9c035ac3b
Amera Ehab <103929057+ameraehab@users.noreply.github.com>
2024-10-20 02:00:49 +0300

    Update resturantDetails.jsx

--- frontend/src/pages/user pages/ResturantDetails/ResturantDetails.jsx
+++ frontend/src/pages/user pages/ResturantDetails/ResturantDetails.jsx
@@ -1,9 +1,9 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
-import './ReturantDetails.css';
+import { faArrowLeft, faClock, faDeafness, faInfo, faInfoCircle, faList, faListAlt, faListDots, faLocation, faLocationArrow, faLocationPin, faMapLocation, faThList, faTimeline, faTimes } from '@fortawesome/free-solid-svg-icons';
 import { useEffect, useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import axios from 'axios';
+import './ReturantDetails.css';
 
  function ResturantDetails(){
     const [restaurantDetails,setRestaurantDetails]=useState(null);
@@ -19,6 +19,7 @@ useEffect(()=>{
                     Authorization: `Bearer ${token}`, 
                 },
             });
+
             setRestaurantDetails(res.data.data.resturant);
             // console.log(res);
         } catch (error) {
@@ -35,14 +36,48 @@ if (!restaurantDetails) {
     return(
         <>
            <div className='container'>
-             <div className='.restaurants-window-Details'></div>
-             <h2><FontAwesomeIcon icon={faArrowLeft}/>Go Back</h2> 
-              <img src={restaurantDetails.imgUrl} alt={restaurantDetails.restaurantName}></img>
+             <div className='restaurants-window-Details'>
+             <h2 onClick={() => navigate(-1)} style={{ cursor: 'pointer' }}  className='backHeader'>
+                
+                <FontAwesomeIcon icon={faArrowLeft}  className='backIcon'/> Go Back
+                </h2 > 
+                <button >Book Now</button>
+                <p className='line'></p>
+                <div className='resturantImg'>
+                <img src={restaurantDetails.imgUrl}></img>
 
-               <h2>{restaurantDetails.restaurantName}</h2>
-              <p>{restaurantDetails.fullAddress}</p>
-              <p>Open Hours: {restaurantDetails.openTime}:00 - {restaurantDetails.closeTime}:00</p>
-              <p>{restaurantDetails.description}</p>
+                </div>
+   
+                  <h2 className='resturantName'>{restaurantDetails.restaurantName}</h2>
+                  <p className='line'></p>
+
+
+                 <div className='details'>
+                    <p> <FontAwesomeIcon icon={faLocationPin} className='icon'/> {restaurantDetails.fullAddress}</p>
+
+                    <p> <FontAwesomeIcon icon={faClock} className='icon'/>Open Hours:<span> {restaurantDetails.openTime}:00</span> AM - <span>{restaurantDetails.closeTime}:00</span> PM</p>
+
+                 <div className='resturantDescription'>
+                   <p> <FontAwesomeIcon icon={faInfoCircle} className='icon'/>{restaurantDetails.description}</p>
+
+                   </div>
+
+                   </div>
+                  <h5>Available Time Slots</h5>                   
+
+                   <div class="time-slots">
+                      <div class="slot">10:30 AM</div>
+                      <div class="slot active">10:45 PM</div>
+                      <div class="slot">11:30 AM</div>
+                      <div class="slot">11:45 AM</div>
+                      <div class="slot active">12:00 PM</div>
+                      <div class="slot active">01:30 PM</div>
+                      <div class="slot">02:30 PM</div>
+                      <div class="slot active">02:45 PM</div>
+                   </div>
+
+             </div>
+            
             </div>
         </>
 

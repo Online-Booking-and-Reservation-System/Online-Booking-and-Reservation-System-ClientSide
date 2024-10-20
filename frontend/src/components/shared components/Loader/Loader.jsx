eeed6fbbca866babef68ee3135da39d9b43a125d
Amera Ehab <103929057+ameraehab@users.noreply.github.com>
2024-10-21 00:31:25 +0300

    Create Loader

---
+++ frontend/src/components/shared components/Loader/Loader.jsx
@@ -0,0 +1,10 @@
+import './Loader.css';
+function Loader (){
+    return(
+        <div className='loader-container'>
+            <p className="loader"></p>
+        </div>
+
+    );
+}
+export default Loader;
